4103dc6b2fd76ac6d356fe0d99aeae61a1d397cf
arnkve <arnas.kvedaras@ktu.edu>
2019-10-11 23:24:57 +0300

    local env set

--- env/Scripts/Activate.ps1
+++ env/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\minda\OneDrive\Documents\pythonPick\env"
+$env:VIRTUAL_ENV="C:\Users\Arnas\Documents\pythonPick\env"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
